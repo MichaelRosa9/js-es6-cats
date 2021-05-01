374c54dd9fafb828540c5a2de4a484919549b8e4
MichaelRosa9 <michaelrosa9@gmail.com>
2021-05-01 11:41:12 +0200

    male female

--- js/script.js
+++ js/script.js
@@ -11,13 +11,16 @@ Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini ma
  */
 
 //funzioni
-const printToPage = ( (object) => {
-  html = $('.box ul').append(`
+const printToPage = ( (object, keyObject) => {
+  
+  let html = `
   <li class="${object.color}">
     <i class="fas fa-cat"></i>
+    <i class="fas fa-ribbon ${keyObject}"></i>
     <span>${object.name}</span>
   </li>
-  `);
+  `;
+  return html;
 })
 
 
@@ -56,7 +59,7 @@ const cats = [
   {
     name: 'Bijou',
     age: 4,
-    color: 'blue',
+    color: 'green',
     gender: 'female'
   }
 ];
@@ -64,10 +67,41 @@ const cats = [
 
 $(document).ready(function(){
 
-
-  cats.forEach((cat) => {
-    printToPage(cat);
+  //milestone 1 con il ciclo forEach svolgo la funzione printToPage che mi appende la lista dentro l'ul
+  cats.forEach( (cat) => {
+    $('#cats-1 ul').append(printToPage(cat));
   });
 
+  //ho definito in due costanti il colore del ribbon
+  const blue = 'blue';
+  const pink = 'pink'
+
+  //faccio un ciclo map() per estrapolare l'array cats e modificarlo aggiungendo dentro ogni oggetto cat un oggetto ribon contente proprieta color e opacity
+  const ribbonCats = cats.map( (cat) => {
+
+    //con il ternay(if di ES6) pongo la condizione che se la proprieta' gendere di cat e' 'male' il colore e' blue. Altrimenti pinki
+     const ribbonColor = (cat.gender === 'male') ? blue : pink;
+
+
+      //con il return clono l'oggetto cat con in aggiunta l'a proprieta'/oggetto ribbon contente le proprieta opacity e ribbonColor
+     return {// chiedere chiarimento
+       ...cat,
+      ribbon:{
+        ribbonColor
+      }}
+    });
+
+
+    //filtro i gatti maschi e i gatti femmina in 2 array
+    const maleCats = ribbonCats.filter((cat)=> cat.gender=== 'male');
+    const femaleCats = ribbonCats.filter((cat)=> cat.gender=== 'female');
+
+    maleCats.forEach((cat) => $('#cats-male ul').append(printToPage(cat, cat.ribbon.ribbonColor)));
+    femaleCats.forEach((cat) => $('#cats-female ul').append(printToPage(cat, cat.ribbon.ribbonColor)));
+    
+
+
+    console.log(maleCats);
+    console.log(femaleCats);
   
 });
